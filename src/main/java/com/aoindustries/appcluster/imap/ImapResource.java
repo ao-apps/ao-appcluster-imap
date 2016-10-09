90e7051bc03713e3a3b4e23254ef3727c7aaad76
AO Industries, Inc. <support@aoindustries.com>
2016-10-08 23:02:17 -0500

    Set top comment correct for this submodule name.

--- src/main/java/com/aoindustries/appcluster/imap/ImapResource.java
+++ src/main/java/com/aoindustries/appcluster/imap/ImapResource.java
@@ -1,24 +1,24 @@
 /*
- * ao-appcluster - Application-level clustering tools.
+ * ao-appcluster-imap - Application-level clustering tools for IMAP account replication.
  * Copyright (C) 2011, 2016  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
  *
- * This file is part of ao-appcluster.
+ * This file is part of ao-appcluster-imap.
  *
- * ao-appcluster is free software: you can redistribute it and/or modify
+ * ao-appcluster-imap is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * ao-appcluster is distributed in the hope that it will be useful,
+ * ao-appcluster-imap is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with ao-appcluster.  If not, see <http://www.gnu.org/licenses/>.
+ * along with ao-appcluster-imap.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.aoindustries.appcluster.imap;
 
