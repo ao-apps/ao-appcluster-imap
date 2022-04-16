d6cccf0ec4c4231eb474af9882c9ec702afd4cc1
AO Industries, Inc <support@aoindustries.com>
2022-04-16 01:35:08 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/aoapps/appcluster/imap/ImapResourceNode.java
+++ src/main/java/com/aoapps/appcluster/imap/ImapResourceNode.java
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with ao-appcluster-imap.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.aoapps.appcluster.imap;
 
 import com.aoapps.appcluster.CronResourceNode;
--- src/main/java/com/aoapps/appcluster/imap/ImapResourcePropertiesConfiguration.java
+++ src/main/java/com/aoapps/appcluster/imap/ImapResourcePropertiesConfiguration.java
@@ -1,6 +1,6 @@
 /*
  * ao-appcluster-imap - Application-level clustering tools for IMAP account replication.
- * Copyright (C) 2011, 2015, 2016, 2019, 2020, 2021  AO Industries, Inc.
+ * Copyright (C) 2011, 2015, 2016, 2019, 2020, 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with ao-appcluster-imap.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.aoapps.appcluster.imap;
 
 import com.aoapps.appcluster.AppCluster;
